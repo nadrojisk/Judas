39a9a505ea49d08e354de95c85a7a940e63489f6
Jordan Sosnowski <jjs0029@auburn.edu>
2020-02-15 18:19:42 -0600

    Add optional tag to make_duplicate

--- src/utilities.py
+++ src/utilities.py
@@ -2,9 +2,14 @@ import os
 import shutil
 
 
-def make_duplicate(source):
+def make_duplicate(source, tag=None):
+    if tag:
+        tag = "_" + tag
+    else:
+        tag = "_"
+
     split_src = source.split('.')
-    split_src[-2] += "_"
+    split_src[-2] += tag
     dest = ".".join(split_src)
     if os.path.exists(dest):
         os.remove(dest)
